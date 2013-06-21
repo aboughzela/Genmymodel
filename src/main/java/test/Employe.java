6718bab767916dc276edfc946a682976de82f2b3
genmymodel <help@genmymodel.com>
2013-06-21 23:46:17 +0200

    commit by genmymodel from www.genmymodel.com: v20130621_234615

---
+++ src/main/java/test/Employe.java
@@ -0,0 +1,40 @@
+package test;
+
+
+/**
+ * <br>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+public class Employe
+{
+	/**
+	 * <br>
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String Name;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String attribute;
+	
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Employe(){
+		
+	}
+
+}
+
